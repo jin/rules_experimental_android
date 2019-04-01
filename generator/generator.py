00d0ad57a7ee3eb67765765f5036e8e64a83bef6
Jingwen Chen <jingwen@google.com>
2019-04-01 15:00:45 -0400

    Fix dependencies in Gradle generator WORKSPACE file

--- generator/generator.py
+++ generator/generator.py
@@ -147,16 +147,16 @@ RULES_JVM_EXTERNAL_TAG = "1.2"
 RULES_JVM_EXTERNAL_SHA = "e5c68b87f750309a79f59c2b69ead5c3221ffa54ff9496306937bfa1c9c8c86b"
 
 local_repository(
-    name = "rules_jvm_external",
-    path = "/Users/jin/code/rules_jvm_external",
+    name = "rules_experimental_android",
+    path = "/usr/local/google/home/jingwen/code/rules_experimental_android",
 )
 
-# http_archive(
-#     name = "rules_jvm_external",
-#     strip_prefix = "rules_jvm_external-%s" % RULES_JVM_EXTERNAL_TAG,
-#     sha256 = RULES_JVM_EXTERNAL_SHA,
-#     url = "https://github.com/bazelbuild/rules_jvm_external/archive/%s.zip" % RULES_JVM_EXTERNAL_TAG,
-# )
+http_archive(
+    name = "rules_jvm_external",
+    strip_prefix = "rules_jvm_external-%s" % RULES_JVM_EXTERNAL_TAG,
+    sha256 = RULES_JVM_EXTERNAL_SHA,
+    url = "https://github.com/bazelbuild/rules_jvm_external/archive/%s.zip" % RULES_JVM_EXTERNAL_TAG,
+)
 
 load("@rules_jvm_external//:defs.bzl", "maven_install")
 maven_install(
